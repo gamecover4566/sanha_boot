f56e8f3587edf7b1e30ff28940ab12c37b169c1d
gamecover4566 <hanju45666@gmail.com>
2020-06-17 23:54:30 +0900

    20200617 spring boot 과제

---
+++ sanha_boot/src/main/java/com/sanha/practice/SanhaBootApplication.java
@@ -0,0 +1,13 @@
+package com.sanha.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanhaBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanhaBootApplication.class, args);
+	}
+
+}
---
+++ sanha_boot/src/test/java/com/sanha/practice/SanhaBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanha.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanhaBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
