c891f1101868b665e637ecfcae82a0961a3baf62
gamecover4566 <hanju45666@gmail.com>
2020-06-17 23:54:30 +0900

    20200607 spring boot 과제

---
+++ sanha_boot/src/main/java/com/sanha/practice/controller/hw1Controller.java
@@ -0,0 +1,53 @@
+package com.sanha.practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class hw1Controller {
+	@GetMapping("/add")
+	public String add(Model model, @RequestParam("val1") int value1, @RequestParam("val2") int value2) {
+		int result = value1 + value2;
+		
+		model.addAttribute("val1", value1);
+		model.addAttribute("val2", value2);
+		model.addAttribute("result", result);
+		
+		return "add";
+	}
+	
+	@GetMapping("/sub")
+	public String sub(Model model, @RequestParam("val1") int value1, @RequestParam("val2") int value2) {
+		int result = value1 - value2;
+		
+		model.addAttribute("val1", value1);
+		model.addAttribute("val2", value2);
+		model.addAttribute("result", result);
+		
+		return "sub";
+	}
+	
+	@GetMapping("/mul")
+	public String mul(Model model, @RequestParam("val1") int value1, @RequestParam("val2") int value2) {
+		int result = value1 * value2;
+		
+		model.addAttribute("val1", value1);
+		model.addAttribute("val2", value2);
+		model.addAttribute("result", result);
+		
+		return "mul";
+	}
+	
+	@GetMapping("/div")
+	public String div(Model model, @RequestParam("val1") int value1, @RequestParam("val2") int value2) {
+		int result = value1 / value2;
+		
+		model.addAttribute("val1", value1);
+		model.addAttribute("val2", value2);
+		model.addAttribute("result", result);
+		
+		return "div";
+	}
+}
